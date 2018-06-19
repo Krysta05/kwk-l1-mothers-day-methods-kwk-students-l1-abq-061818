08e007a39a9475872f207806cfa1a4025e9ddc84
Krysta05 <martinezkrysta7@gmail.com>
2018-06-19 17:06:10 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -2,7 +2,4 @@
 def mothers_day(mom) 
   puts "Happy Mothers Day, #{mom}!"
 end
-mothers_day("Beyonce")
-def motherss_day("mom","names")
-  puts "Happy Mothers Day, #{mom},#{names}"
-  mothers_day("mom" "Beyonce")
+mothers_day("beyonce")
